6b66710836232f50221bac7ea81334dad70253bd
Bleno Vale <Bleno Vale@Bleno>
2016-02-27 13:36:58 -0300

    Implementação do QuickSort

--- Analise de Algoritmos/src/main/Principal.java
+++ Analise de Algoritmos/src/main/Principal.java
@@ -7,21 +7,35 @@ package main;
 public class Principal {
 
     Ordenacao ord = new Ordenacao();
+    int[] input = {24, 2, 45, 20, 56, 75, 2, 56, 99, 53, 12};
 
     public Principal() {
 
-        // questao1();
-        // questao2();
-        // questao3();
+        questao1(input);
+        printArray();
+        
+//        questao2();
+//        questao3();
+
     }
 
-    private void questao1() {
+    private void questao1(int[] input) {
+        new QuickSort().sort(input);
     }
 
     private void questao2() {
     }
 
     private void questao3() {
+    }
+
+    private void printArray() {
+        System.out.print("Vetor de Saída: ");
+        for (int i : input) {
+            System.out.print(i);
+            System.out.print(" ");
+        }
+        System.out.print("\n\n");
     }
 
     public static void main(String[] args) {
