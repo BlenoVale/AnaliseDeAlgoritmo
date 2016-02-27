a185f87a4bea6fbf02f7cd6d54a9b7b32636a8cd
Dan Jhonatan <>
2016-02-27 09:53:24 -0300

    Preparação das classes

---
+++ Analise de Algoritmos/src/main/Ordenacao.java
@@ -0,0 +1,25 @@
+package main;
+
+/**
+ *
+ * @author Dan Jhonatan
+ */
+public class Ordenacao {
+    
+    // Qualquer outro mehtodo interno que nao seja algoritmo de ordenacaoh deve ser private
+
+    public void quickSort() {
+    }
+
+    public void heapSort() {
+    }
+
+    public void countingSort() {
+    }
+
+    public void radixSort() {
+    }
+
+    public void bucketSort() {
+    }
+}
--- Analise de Algoritmos/src/main/Principal.java
+++ Analise de Algoritmos/src/main/Principal.java
@@ -5,5 +5,26 @@ package main;
  * @author Bleno, Dan Jhonatan, Daniel
  */
 public class Principal {
-    
+
+    Ordenacao ord = new Ordenacao();
+
+    public Principal() {
+
+        // questao1();
+        // questao2();
+        // questao3();
+    }
+
+    private void questao1() {
+    }
+
+    private void questao2() {
+    }
+
+    private void questao3() {
+    }
+
+    public static void main(String[] args) {
+        new Principal();
+    }
 }
